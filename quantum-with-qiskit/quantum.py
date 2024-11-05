0cd6ca79a0e89fd1c12a69d3ee27b9c89530e8eb
Saba Bozpolat <34461238+sbbzplt@users.noreply.github.com>
2024-11-05 18:41:52 +0300

    Update quantum.py

--- quantum-with-qiskit/quantum.py
+++ quantum-with-qiskit/quantum.py
@@ -6,3 +6,5 @@ from drawing import draw_axes, draw_unit_circle, draw_quantum_state, draw_qubit,
 from quantum_state import random_qstate_by_value, random_qstate_by_angle, angle_qstate
 
 from grover import giant_oracle, giant_oracle2, giant_diffusion, Uf, Uf_8
+
+from quantum_operator import *
