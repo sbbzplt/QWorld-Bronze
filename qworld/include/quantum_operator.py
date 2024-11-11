d534a24d16ff94cd543be500e82e9a73df0c9d17
Saba Bozpolat <34461238+sbbzplt@users.noreply.github.com>
2024-11-11 19:49:10 +0300

    Update quantum_operator.py

--- qworld/include/quantum_operator.py
+++ qworld/include/quantum_operator.py
@@ -1,21 +1,11 @@
-import random, math, numpy
-from quantum_state import random_qstate_by_value
+import numpy as np
+from scipy.linalg import qr
 
 # randomly create a 2x2-dimensional un,tary operator
-def random_unitary_2x2():
-    first_col = random_qstate_by_value()
-    second_col = random_qstate_by_value()
-    unitary = [[first_col[0], second_col[0]], 
-               [first_col[1], second_col[1]]]
+def random_unitary(n=2):    
+    H = np.random.randn(n, n)
+    Q, R = qr(H)
+    unitary = list(Q.dot(Q.T))
     return unitary
 
-# randomly create a 4x4-dimensional un,tary operator
-def random_unitary(qubit_num=2):
-    N = int(numpy.ceil(2 ** qubit_num))
-    unitary = numpy.array([[numpy.random.randint(0, 100) for _ in range(N)] for _ in range(N)], dtype="float64")
-    for i in range(N):
-        col = unitary[:, i]
-        length = numpy.sqrt(sum([j ** 2 for j in col]))
-        col /= length
-        unitary[:, i] = col
-    return unitary
+
