0204038aae2331a4b1d5e8643a61f2a48c127016
Saba Bozpolat <34461238+sbbzplt@users.noreply.github.com>
2024-11-11 19:51:08 +0300

    Update quantum_operator.py

--- qworld/include/quantum_operator.py
+++ qworld/include/quantum_operator.py
@@ -3,7 +3,8 @@ from scipy.linalg import qr
 
 # randomly create a 2x2-dimensional un,tary operator
 def random_unitary(n=2):    
-    H = np.random.randn(n, n)
+    N = int(2 ** n)    
+    H = np.random.randn(N, N)
     Q, R = qr(H)
     unitary = list(Q.dot(Q.T))
     return unitary
