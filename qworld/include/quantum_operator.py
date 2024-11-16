8e7a27c72ec7acdd976a556be8ec3a61450d0b9d
Saba Bozpolat <34461238+sbbzplt@users.noreply.github.com>
2024-11-16 20:12:37 +0300

    Update quantum_operator.py

--- qworld/include/quantum_operator.py
+++ qworld/include/quantum_operator.py
@@ -8,4 +8,22 @@ def random_unitary(n=2):
     Q, R = qr(H)
     return Q
 
+def random_complex_unitary(eps=0.0001):
+    alpha = np.random.random() * 2 * np.pi
+    beta = np.random.random() * 2 * np.pi
+    theta = np.random.random() * 2 * np.pi
+    u = np.array([[np.exp(1j*alpha)*np.cos(theta), np.exp(1j*beta)*np.sin(theta)], 
+                  [-np.exp(-1j*beta)*np.sin(theta), np.exp(-1j*beta)*np.cos(theta)]])
+    res = np.matmul(u, np.conjugate(np.transpose(u)))
+    while not (abs(res[0, 0] - 1) < eps and abs(res[1, 1] - 1) < eps and abs(res[0, 1]) < eps and abs(res[1, 0]) < eps):
+        alpha = random() * 2 * np.pi
+        beta = random() * 2 * np.pi
+        theta = random() * 2 * np.pi
+    
+        u = np.array([[np.exp(1j*alpha)*np.cos(theta), np.exp(1j*beta)*np.sin(theta)], 
+                    [-np.exp(-1j*beta)*np.sin(theta), np.exp(-1j*beta)*np.cos(theta)]])
+    
+        res = np.matmul(u, np.conjugate(np.transpose(u)))
+    return u
+
 
