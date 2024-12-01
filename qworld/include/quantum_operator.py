c6ea68f052d68c2a93be30f9a5484a7d7484f9cd
Saba Bozpolat <34461238+sbbzplt@users.noreply.github.com>
2024-12-01 20:28:32 +0300

    Update quantum_operator.py

--- qworld/include/quantum_operator.py
+++ qworld/include/quantum_operator.py
@@ -1,29 +1,17 @@
 import numpy as np
 from scipy.linalg import qr
 
-# randomly create a 2x2-dimensional un,tary operator
-def random_unitary(n=2):    
-    N = int(2 ** n)    
-    H = np.random.randn(N, N)
-    Q, R = qr(H)
+# randomly create a NxN-dimensional unitary operator
+def random_unitary(num_qubit=1, dtype="float"):
+    if dtype == "float":
+        N = int(2 ** num_qubit)    
+        H = np.random.randn(N, N)
+        Q, R = qr(H)
+    elif dtype == "complex":
+        N = int(2 ** num_qubit)    
+        H_real = np.random.randn(N, N)
+        H_imag = np.random.randn(N, N)
+        Q, R = qr(H_real + 1j*H_imag)
+    else:
+        raise AttributionError(f"dtype should be either 'float' or 'complex', not {dtype}")
     return Q
-
-def random_complex_unitary(eps=0.0001):
-    alpha = np.random.random() * 2 * np.pi
-    beta = np.random.random() * 2 * np.pi
-    theta = np.random.random() * 2 * np.pi
-    u = np.array([[np.exp(1j*alpha)*np.cos(theta), np.exp(1j*beta)*np.sin(theta)], 
-                  [-np.exp(-1j*beta)*np.sin(theta), np.exp(-1j*beta)*np.cos(theta)]])
-    res = np.matmul(u, np.conjugate(np.transpose(u)))
-    while not (abs(res[0, 0] - 1) < eps and abs(res[1, 1] - 1) < eps and abs(res[0, 1]) < eps and abs(res[1, 0]) < eps):
-        alpha = random() * 2 * np.pi
-        beta = random() * 2 * np.pi
-        theta = random() * 2 * np.pi
-    
-        u = np.array([[np.exp(1j*alpha)*np.cos(theta), np.exp(1j*beta)*np.sin(theta)], 
-                    [-np.exp(-1j*beta)*np.sin(theta), np.exp(-1j*beta)*np.cos(theta)]])
-    
-        res = np.matmul(u, np.conjugate(np.transpose(u)))
-    return u
-
-
